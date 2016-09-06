b9ed66453d86ea496494278b1e1442f82753bc90
Victorien <victorien.derasse@hotmail.com>
2016-09-06 16:52:29 +0200

    commit 1

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.use(function(err, req, res, next) {
 const connection = mysql.createConnection({
   host : '192.168.1.100',
   user : 'root',
-  password : '***',
+  password : 'tfepassword',
   database : 'TFE'
 });
 
