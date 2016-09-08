f3ede42a11fd67e443dee2007f0572f757620468
Victorien <victorien.derasse@hotmail.com>
2016-09-08 15:32:34 +0200

    fs readdir

--- public/javascripts/server.js
+++ public/javascripts/server.js
@@ -236,11 +236,9 @@ module.exports = function(socket, io, connection, fs) {
 
     socket.on('setProcessPID', function(data){
         console.log('setProcessPID event');
-        console.log(data.pid+data.cameraName);
         const setPID = 'UPDATE camera SET process = '+data.pid+' WHERE name = '+data.cameraName;
         connection.query(setPID,function(err){
             if(err){
-                console.log('setPID MYSQL error : '+err);
                 throw err;
             }
         });
