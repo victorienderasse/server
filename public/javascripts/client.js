52bc19ee0d2f341998da7b1d1fcfa93cf2a62292
Victorien <victorien.derasse@hotmail.com>
2016-12-02 14:50:40 +0100

    fs readdir

---
+++ public/javascripts/client.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Victorien on 02-12-16.
+ */
+var socket = io.connect(serverURL);
+
+socket.emit('client','client');
+
+socket.on('sendCamera', function(data){
+    tbScreen = data;
+});
+
+socket.on('redirect', function(url){
+    window.location = url;
+});
