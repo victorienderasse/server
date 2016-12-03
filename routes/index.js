223443539b301d31993b079e94beca482bee8815
Victorien <victorien.derasse@hotmail.com>
2016-12-03 13:15:43 +0100

    fs readdir

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { name: 'Index' });
 });
 
-router.get('/display', function(req,res,next){
+router.get('/display', function(req,res){
  var userID = req.query.userID;
   res.render('display',{userID: userID});
 });
