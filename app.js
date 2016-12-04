355efb47b773ecb972b6a57e6021960c15f8f886
Victorien <victorien.derasse@hotmail.com>
2016-12-04 15:32:52 +0100

    fs readdir

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+//app.set('view engine', 'ejs');
 app.set('port', port);
 
 
