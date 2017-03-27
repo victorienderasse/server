e8deae544be7139d34abe35d6a487b51aa15622c
Victorien <victorien.derasse@hotmail.com>
2017-03-27 13:54:02 +0200

    live recording test

--- app.js
+++ app.js
@@ -583,9 +583,12 @@ io.sockets.on('connection', function(socket){
         throw err;
       }
       if(rows.length>0){
+        console.log('camera state : '+rows[0].state);
         if(rows[0].state == 2){
-          console.log('camera state : '+rows[0].state);
+          console.log('stream launch');
           socket.emit('startStream', {cameraID: cameraID, name: rows[0].name});
+        }else{
+          console.log('state != 2');
         }
       }else{
         console.log('Error getInfoCamera in getLiveRecordingDone');
