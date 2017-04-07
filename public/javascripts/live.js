bb97ea73c25130f80eae91514eb6f944947211d1
Victorien <victorien.derasse@hotmail.com>
2017-04-07 19:03:46 +0200

    display update

--- public/javascripts/live.js
+++ public/javascripts/live.js
@@ -168,12 +168,12 @@ function displayCamera(tbCamera){
             row = document.createElement('div');
             row.id = 'row'+nbRow;
             row.className = 'row';
-            camera.className = 'col-lg-offset-2 col-lg-4 camera-display';
+            camera.className = 'col-lg-offset-1 col-lg-4 camera-display';
             row.appendChild(camera);
             display.appendChild(row);
         }else{
             row = document.getElementById('row'+nbRow);
-            camera.className = 'col-lg-offset-1 col-lg-4 camera-display';
+            camera.className = 'col-lg-offset-2 col-lg-4 camera-display';
             row.appendChild(camera);
         }
 
