42674bfdd0d886fede174bbb670c50830a2970d3
Victorien <victorien.derasse@hotmail.com>
2017-04-08 16:28:01 +0200

    display update

--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -443,7 +443,7 @@ function displayCamera(tbCamera){
             row.id = 'row'+nbRow;
             row.className = 'row';
             row.setAttribute('style','margin-bottom:3%;');
-            camera.className = 'col-lg-offset-1 col-lg-4 camera-display fade';
+            camera.className = 'col-lg-offset-1 col-lg-4 camera-display';
             row.appendChild(camera);
             display.appendChild(row);
         }else{
