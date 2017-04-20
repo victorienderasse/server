135776bb26daf1c132d88dbf6695788b9dbc66da
Victorien <victorien.derasse@hotmail.com>
2017-04-20 16:35:07 +0200

    qrcode

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -6,40 +6,63 @@ var userID = document.getElementById('userID').innerHTML;
 //socket.emit('getInfoUser',userID);
 
 $(function(){
-    $('#test').fadeOut('slow', function(){
-        $(this).fadeIn('slow');
+    $('#option, #form').hide();
+
+    $('#affiche').click(function(){
+        $('#option').first().show('slow');
+    });
+
+    $('#displayForm').click(function(){
+        $('#user').hide('slow', function(){
+            $('#form').show('slow');
+        });
     })
 });
 
 socket.on('getInfoUserRes', function(data){
     console.log('getInfoUserRes event');
 
-    document.getElementById('userName').innerHTML = data[0].userName;
-    document.getElementById('userEmail').innerHTML = data[0].email;
-    if(data[0].phone == '' || data[0].phone == 'undefined' || data[0].phone == null){
-        document.getElementById('userPhone').innerHTML = 'No phone number';
-    }else{
-        document.getElementById('userPhone').innerHTML = data[0].phone;
-    }
+    $('#userName').html('<b>'+data[0].userName+'</b>');
+    $('#userEmail').html('<b>'+data[0].email+'</b>');
+    $('#userPhone').html('<b>'+data[0].phone+'</b>');
 
-    var table = document.getElementById('cameraList');
+
+    var table = document.getElementById('infoCamera');
     for(var i=0;i<data.length;i++){
-        var camera = document.createElement('tr');
-        var nameTD = document.createElement('td');
-        var enableTD = document.getElementById('td');
+        var camera = document.createElement('div');
+        var WifiDiv = document.createElement('div');
+        var serialDiv = document.createElement('div');
+        var name = document.createTextNode('camera'+data[i].cameraName);
+        camera.appendChild(name);
+        var btn = document.createElement('button');
+        var btnIcon = document.createElement('span');
+        btnName.className = 'glyphicon glyphicon-chevron-down';
+        btn.appendChild(btnName);
+        camera.appendChild(btn);
+        var wifi = document.createTextNode('Add Wifi Network');
+        var wifiIcon = document.createElement('span');
+        wifiIcon.className = 'glyphicon glyphicon-plus-sign';
+        WifiDiv.appendChild(wifi);
+        var serial = document.createTextNode(data[i].serial);
+        serialDiv.appendChild(serial);
+        var option = document.createElement('div');
+        option.appendChild(WifiDiv);
+        option.appendChild(serialDiv);
+
+        table.appendChild(camera);
+        table.appendChild(option);
     }
 
 });
-
-
-function displayForm(){
-    document.getElementById('displayUser').setAttribute('style','display:none;');
-    document.getElementById('displayForm').setAttribute('style','display:block;');
-}
 
 
 function updateUser(){
     $(function(){
         $('#test').slideToggle();
     });
+}
+
+
+function click(){
+    $('span').slideDown();
 }
