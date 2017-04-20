ded7395305a466823d1281131a61425063fa9c76
Victorien <victorien.derasse@hotmail.com>
2017-04-20 12:50:15 +0200

    qrcode

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -6,7 +6,7 @@ var userID = document.getElementById('userID').innerHTML;
 //socket.emit('getInfoUser',userID);
 
 $(function(){
-    $('userName').html('test');
+    $('test').html('test');
 });
 
 socket.on('getInfoUserRes', function(data){
