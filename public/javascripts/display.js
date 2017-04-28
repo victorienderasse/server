8e65bb4a7f178b058575d2a61171120a8f0e00f1
Victorien <victorien.derasse@hotmail.com>
2017-04-28 11:16:02 +0200

    Update timer interface

--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -597,13 +597,13 @@ function displayCamera(tbCamera){
             row.id = 'row'+nbRow;
             row.className = 'row';
             row.setAttribute('style','margin-bottom:3%;');
-            camera.className = 'col-lg-offset-1 col-lg-4 camera-display';
+            camera.addClass('col-lg-offset-1 col-lg-4');
             row.appendChild(camera);
             display.appendChild(row);
         }else{
             row = document.getElementById('row'+nbRow);
             row.setAttribute('style','margin-bottom:3%;');
-            camera.className = 'col-lg-offset-2 col-lg-4 camera-display';
+            camera.addClass('col-lg-offset-2 col-lg-4');
             row.appendChild(camera);
         }
 
