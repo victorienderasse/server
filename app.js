c924aefc08a035b615b9dc185d814134959a10dc
Victorien <victorien.derasse@hotmail.com>
2017-05-01 14:30:16 +0200

    Update timer interface

--- app.js
+++ app.js
@@ -810,7 +810,7 @@ io.sockets.on('connection', function(socket){
   socket.on('addOrder', function(data){
     console.log('addOrder event');
     const addOrder = 'INSERT INTO order (userID, state, date) VALUES ('+data.userID+', 0, CURRENT_TIMESTAMP)';
-    connection(addOrder, function(err){
+    connection.query(addOrder, function(err){
       if(err)throw err;
       const getOrderID = 'SELECT orderID FROM order WHERE userID = '+data.userID+' ORDER BY orderID LIMIT 1';
       connection.query(getOrderID, function(err,rows){
