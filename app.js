b8529df7f51da683788994c83e94ecb2d71c83d2
Victorien <victorien.derasse@hotmail.com>
2017-05-05 12:00:54 +0200

    Update timer interface

--- app.js
+++ app.js
@@ -651,17 +651,19 @@ io.sockets.on('connection', function(socket){
 
   socket.on('stopMultiLive', function(userID){
     console.log('stopMultiLive event');
+    var ind;
     const getCamera = 'SELECT * FROM camera WHERE userID = '+userID;
     connection.query(getCamera, function(err,rows){
       if(err)throw err;
       if(rows.length>0){
         for(var i =0;i<rows.length;i++){
-
+          ind = i;
           setRecordUnpaused(rows[i].cameraID);
 
           getInfoCamera(rows[i].cameraID, function(camera){
 
-            if(rows[i].state == 4){
+
+            if(rows[ind].state == 4){
               sendToCamera(camera.cameraID,'getLiveRecording',{cameraID:camera.cameraID, name: camera.name});
             }else{
               sendToCamera(camera.cameraID,'killProcess',null);
